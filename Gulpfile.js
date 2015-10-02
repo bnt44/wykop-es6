04966708e53f5feadb0fa35e73cf60260fde5509
bnt44 <bnt44_npm@wp.pl>
2015-10-02 23:18:46 +0200

    revert uglify

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,9 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
-var uglify = require('gulp-uglify');
 
 gulp.task('default', function () {
 	return gulp.src('lib/*.js')
 		.pipe(babel())
-		.pipe(uglify())
 		.pipe(gulp.dest('dist'));
 });
 
--- dist/wykop-es6.js
+++ dist/wykop-es6.js
@@ -1,1 +1,260 @@
-"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{"default":e}}function _classCallCheck(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}Object.defineProperty(exports,"__esModule",{value:!0});var _createClass=function(){function e(e,t){for(var r=0;r<t.length;r++){var a=t[r];a.enumerable=a.enumerable||!1,a.configurable=!0,"value"in a&&(a.writable=!0),Object.defineProperty(e,a.key,a)}}return function(t,r,a){return r&&e(t.prototype,r),a&&e(t,a),t}}(),_lodash=require("lodash"),_lodash2=_interopRequireDefault(_lodash),_assert=require("assert"),_assert2=_interopRequireDefault(_assert),_cryptoJsMd5=require("crypto-js/md5"),_cryptoJsMd52=_interopRequireDefault(_cryptoJsMd5),_request=require("request"),_request2=_interopRequireDefault(_request),_methodsJs=require("./methods.js"),_methodsJs2=_interopRequireDefault(_methodsJs),f=function(){},Wykop=function(){function e(t,r){var a=arguments.length<=2||void 0===arguments[2]?{}:arguments[2],s=a.output,o=a.format,u=a.timeout,n=void 0===u?3e4:u,i=a.useragent,l=void 0===i?"WypokAgent":i,d=a.userkey,f=a.info;_classCallCheck(this,e),_assert2["default"](t&&r,"appkey and secretkey cannot be null"),Object.assign(this,{appkey:t,secretkey:r,output:s,format:o,timeout:n,useragent:l,userkey:d,info:f})}return _createClass(e,[{key:"get",value:function(t,r){var a=arguments.length<=2||void 0===arguments[2]?{}:arguments[2],s=a.params,o=a.api,u=a.post,n=arguments.length<=3||void 0===arguments[3]?f:arguments[3];_assert2["default"](t&&r,"rtype and rmethod must be String and cannot be null");var i=this.appkey,l=this.secretkey,d=this.userkey,m=this.output,h=this.format,p=this.timeout,c=this.useragent,y=_lodash2["default"](s).isEmpty()?"":s.join("/")+"/",g=e.parseApi({appkey:i,userkey:d,output:m,format:h},o),_=e.parsePostParams(u),v=_.form,k=_.formData,b=_.sortedPost,q="http://a.wykop.pl/"+t+"/"+r+"/"+y+g,P={url:q,method:_lodash2["default"](u).isEmpty()?"GET":"POST",json:!0,timeout:p,headers:{"User-Agent":c,apisign:_cryptoJsMd52["default"](l+q+b).toString()},form:v,formData:k};return new Promise(function(e,t){_request2["default"](P,function(r,a,s){r?(t(r),n(r)):a.statusCode>=200&&a.statusCode<300?s.error?(n(s.error),t(s.error)):(n(null,s),e(s)):(t(a),n(a))})})}},{key:"login",value:function(t){var r=arguments.length<=1||void 0===arguments[1]?f:arguments[1];_assert2["default"](t,"accountkey cannot be null");var a=this.appkey,s=this.secretkey,o=this.output,u=this.format,n=this.timeout,i=this.useragent;return this.get("User","Login",{post:{accountkey:t}}).then(function(t){var l=t.userkey,d=new e(a,s,{output:o,format:u,timeout:n,useragent:i,userkey:l,info:t});return r(null,d),Promise.resolve(d)})}}],[{key:"parseApi",value:function(e,t){Object.assign(e,t);var r=_lodash2["default"](e).omit(_lodash2["default"].isUndefined).omit(_lodash2["default"].isNull).keys();return _lodash2["default"](r).reduce(function(t,a,s){return t+a+","+e[a]+(s===r.length-1?"":",")},"")}},{key:"parsePostParams",value:function(){var e=arguments.length<=0||void 0===arguments[0]?{}:arguments[0],t=void 0,r=void 0,a=void 0;return e.embed&&"string"!=typeof e.embed?(r=e,a=function(){var t=_lodash2["default"].omit(e,"embed");return _lodash2["default"](t).omit(_lodash2["default"].isUndefined).omit(_lodash2["default"].isNull).sortBy(function(e,t){return t}).toString()}()):_lodash2["default"](e).isEmpty()?a="":(t=e,a=_lodash2["default"](e).omit(_lodash2["default"].isUndefined).omit(_lodash2["default"].isNull).sortBy(function(e,t){return t}).toString()),{form:t,formData:r,sortedPost:a}}},{key:"createMethods",value:function(){var t=!0,r=!1,a=void 0;try{for(var s,o=function(){var t=s.value;e.prototype[t.name]=function(){var e=arguments.length<=0||void 0===arguments[0]?{}:arguments[0],r=arguments.length<=1||void 0===arguments[1]?f:arguments[1];return this.get(t.type,t.method,e,r)}},u=_methodsJs2["default"][Symbol.iterator]();!(t=(s=u.next()).done);t=!0)o()}catch(n){r=!0,a=n}finally{try{!t&&u["return"]&&u["return"]()}finally{if(r)throw a}}}}]),e}();Wykop.createMethods(),exports["default"]=Wykop,module.exports=exports["default"];
+'use strict';
+Object.defineProperty(exports, '__esModule', {
+	value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+var _assert = require('assert');
+
+var _assert2 = _interopRequireDefault(_assert);
+
+var _cryptoJsMd5 = require('crypto-js/md5');
+
+var _cryptoJsMd52 = _interopRequireDefault(_cryptoJsMd5);
+
+var _request = require('request');
+
+var _request2 = _interopRequireDefault(_request);
+
+var _methodsJs = require('./methods.js');
+
+var _methodsJs2 = _interopRequireDefault(_methodsJs);
+
+var f = function f() {};
+
+var Wykop = (function () {
+
+	/**
+ * @param {string} appkey    Klucz API
+ * @param {string} secretkey Sekret aplikacji
+ * // todo reszta
+ */
+
+	function Wykop(appkey, secretkey) {
+		var _ref = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
+
+		var output = _ref.output;
+		var format = _ref.format;
+		var _ref$timeout = _ref.timeout;
+		var timeout = _ref$timeout === undefined ? 30000 : _ref$timeout;
+		var _ref$useragent = _ref.useragent;
+		var useragent = _ref$useragent === undefined ? 'WypokAgent' : _ref$useragent;
+		var userkey = _ref.userkey;
+		var info = _ref.info;
+
+		_classCallCheck(this, Wykop);
+
+		(0, _assert2['default'])(appkey && secretkey, 'appkey and secretkey cannot be null');
+		Object.assign(this, { appkey: appkey, secretkey: secretkey, output: output, format: format, timeout: timeout, useragent: useragent, userkey: userkey, info: info });
+	}
+
+	/*
+ * Generujemy metody!
+ */
+
+	/**
+ * Zmiana parametrów API w string
+ * @param {Object} base 
+ * @param {Object} api parametry API
+ */
+
+	_createClass(Wykop, [{
+		key: 'get',
+
+		/**
+  * Tworzenie requestu do API
+  * @param {string}   rtype        Nazwa zasobu np. 'Link'
+  * @param {string}   rmethod      Nazwa metody np. 'Index'
+  * @param {string[]} params Parametry metody np. ['14278527']
+  * @param {Object}   api    Parametry api np. {page: 1}
+  * @param {Object}   post   Parametry POST np. {body: 'string'}
+  */
+		value: function get(rtype, rmethod) {
+			var _ref2 = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
+
+			var params = _ref2.params;
+			var api = _ref2.api;
+			var post = _ref2.post;
+			var callback = arguments.length <= 3 || arguments[3] === undefined ? f : arguments[3];
+
+			(0, _assert2['default'])(rtype && rmethod, 'rtype and rmethod must be String and cannot be null');
+
+			var appkey = this.appkey;
+			var secretkey = this.secretkey;
+			var userkey = this.userkey;
+			var output = this.output;
+			var format = this.format;
+			var timeout = this.timeout;
+			var useragent = this.useragent;
+
+			var _params = !(0, _lodash2['default'])(params).isEmpty() ? params.join('/') + '/' : ''; // zmiana tablicy z parametrami metody w string
+			var _api = Wykop.parseApi({ appkey: appkey, userkey: userkey, output: output, format: format }, api); // zmiana obiektu z parametrami api w string
+
+			var _Wykop$parsePostParams = Wykop.parsePostParams(post);
+
+			var form = _Wykop$parsePostParams.form;
+			var formData = _Wykop$parsePostParams.formData;
+			var sortedPost = _Wykop$parsePostParams.sortedPost;
+			// parsowanie parametrów POST
+
+			// tworzymy url zapytania
+			var url = 'http://a.wykop.pl/' + rtype + '/' + rmethod + '/' + _params + _api;
+
+			var options = {
+				url: url,
+				method: !(0, _lodash2['default'])(post).isEmpty() ? 'POST' : 'GET',
+				json: true,
+				timeout: timeout,
+				headers: {
+					'User-Agent': useragent,
+					'apisign': (0, _cryptoJsMd52['default'])(secretkey + url + sortedPost).toString()
+				},
+				form: form,
+				formData: formData
+			};
+
+			/*
+   * Wykonujemy request, metoda get zwraca promise
+   */
+			return new Promise(function (resolve, reject) {
+				(0, _request2['default'])(options, function (error, response, body) {
+
+					if (error) {
+						reject(error);
+						callback(error);
+					} else if (!(response.statusCode >= 200 && response.statusCode < 300)) {
+						reject(response);
+						callback(response);
+					} else if (body.error) {
+						callback(body.error);
+						reject(body.error);
+					} else {
+						callback(null, body);
+						resolve(body);
+					}
+				});
+			});
+		}
+
+		/*
+  * @param {String} accountkey Klucz połączenia konta z aplikacją, zwraca nowy obiekt User
+  */
+	}, {
+		key: 'login',
+		value: function login(accountkey) {
+			var callback = arguments.length <= 1 || arguments[1] === undefined ? f : arguments[1];
+
+			(0, _assert2['default'])(accountkey, 'accountkey cannot be null');
+			var appkey = this.appkey;
+			var secretkey = this.secretkey;
+			var output = this.output;
+			var format = this.format;
+			var timeout = this.timeout;
+			var useragent = this.useragent;
+
+			return this.get('User', 'Login', { post: { accountkey: accountkey } }).then(function (res) {
+				var userkey = res.userkey;
+				var user = new Wykop(appkey, secretkey, { output: output, format: format, timeout: timeout, useragent: useragent, userkey: userkey, info: res });
+				callback(null, user);
+				return Promise.resolve(user);
+			});
+		}
+
+		/*
+  * Kreator metod uproszczonych na podstawie ./methods.js
+  */
+	}], [{
+		key: 'parseApi',
+		value: function parseApi(base, api) {
+			Object.assign(base, api);
+			var keys = (0, _lodash2['default'])(base).omit(_lodash2['default'].isUndefined).omit(_lodash2['default'].isNull).keys();
+			return (0, _lodash2['default'])(keys).reduce(function (memo, key, index) {
+				return memo + key + ',' + base[key] + (index === keys.length - 1 ? '' : ',');
+			}, '');
+		}
+	}, {
+		key: 'parsePostParams',
+		value: function parsePostParams() {
+			var post = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+
+			var form = undefined,
+			    formData = undefined,
+			    sortedPost = undefined;
+
+			if (post.embed && typeof post.embed !== 'string') {
+				formData = post;
+				sortedPost = (function () {
+					var _post = _lodash2['default'].omit(post, 'embed');
+					return (0, _lodash2['default'])(_post).omit(_lodash2['default'].isUndefined).omit(_lodash2['default'].isNull).sortBy(function (val, key) {
+						return key;
+					}).toString();
+				})();
+			} else if (!(0, _lodash2['default'])(post).isEmpty()) {
+				form = post;
+				sortedPost = (0, _lodash2['default'])(post).omit(_lodash2['default'].isUndefined).omit(_lodash2['default'].isNull).sortBy(function (val, key) {
+					return key;
+				}).toString();
+			} else {
+				sortedPost = "";
+			}
+			return { form: form, formData: formData, sortedPost: sortedPost };
+		}
+	}, {
+		key: 'createMethods',
+		value: function createMethods() {
+			var _iteratorNormalCompletion = true;
+			var _didIteratorError = false;
+			var _iteratorError = undefined;
+
+			try {
+				var _loop = function () {
+					var item = _step.value;
+
+					Wykop.prototype[item.name] = function () {
+						var obj = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+						var callback = arguments.length <= 1 || arguments[1] === undefined ? f : arguments[1];
+
+						//assert(this.userkey, 'Nie jesteś zalogowany');
+						return this.get(item.type, item.method, obj, callback);
+					};
+				};
+
+				for (var _iterator = _methodsJs2['default'][Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+					_loop();
+				}
+			} catch (err) {
+				_didIteratorError = true;
+				_iteratorError = err;
+			} finally {
+				try {
+					if (!_iteratorNormalCompletion && _iterator['return']) {
+						_iterator['return']();
+					}
+				} finally {
+					if (_didIteratorError) {
+						throw _iteratorError;
+					}
+				}
+			}
+		}
+	}]);
+
+	return Wykop;
+})();
+
+Wykop.createMethods();
+
+/**
+* exportujemy klasę Wykop jako default
+*/
+exports['default'] = Wykop;
+module.exports = exports['default'];
