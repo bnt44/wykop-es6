ceb9a85541b7b6319df8106f6d671f33c31e2e61
apopek <arturpopek.ap@gmail.com>
2018-01-11 15:55:58 +0100

    change Promises to Bluebird

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,23 @@
 'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var _ = require('lodash');
 var assert = require('assert');
 var crypto = require('crypto');
 var doRequest = require('request');
-var es6Promise = require('es6-promise');
-
-var _Promise = typeof Promise === 'undefined' ? es6Promise.Promise : Promise;
+var _Promise = require('bluebird');
 
 // create md5 hash
 function md5() {
-	var string = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
+	var string = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
 
 	return crypto.createHash('md5').update(new Buffer(string, 'utf-8')).digest("hex");
 }
 
-var Wykop = (function () {
+var Wykop = function () {
 
 	/**
  * @param {string} appkey    Klucz API
@@ -30,24 +28,22 @@ var Wykop = (function () {
  * @param {string} useragent Useragent pod jakim przedstawiamy się serwerowi
  * @param {boolean} ssl Jezeli true requesty będą wysyłane pod szyfrowany adres
  */
-
 	function Wykop(appkey, secretkey) {
-		var _ref = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
-
-		var output = _ref.output;
-		var format = _ref.format;
-		var _ref$timeout = _ref.timeout;
-		var timeout = _ref$timeout === undefined ? 30000 : _ref$timeout;
-		var _ref$useragent = _ref.useragent;
-		var useragent = _ref$useragent === undefined ? 'WypokAgent' : _ref$useragent;
-		var _ref$ssl = _ref.ssl
-		var ssl = _ref$ssl === undefined ? false : _ref$ssl;
-		var accountkey = _ref.accountkey;
-		var userkey = _ref.userkey;
-		var _ref$autologin = _ref.autologin;
-		var autologin = _ref$autologin === undefined ? false : _ref$autologin;
-		var _ref$retryCount = _ref.retryCount;
-		var retryCount = _ref$retryCount === undefined ? 1 : _ref$retryCount;
+		var _ref = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {},
+		    output = _ref.output,
+		    format = _ref.format,
+		    _ref$timeout = _ref.timeout,
+		    timeout = _ref$timeout === undefined ? 30000 : _ref$timeout,
+		    _ref$useragent = _ref.useragent,
+		    useragent = _ref$useragent === undefined ? 'WypokAgent' : _ref$useragent,
+		    _ref$ssl = _ref.ssl,
+		    ssl = _ref$ssl === undefined ? false : _ref$ssl,
+		    accountkey = _ref.accountkey,
+		    userkey = _ref.userkey,
+		    _ref$autologin = _ref.autologin,
+		    autologin = _ref$autologin === undefined ? false : _ref$autologin,
+		    _ref$retryCount = _ref.retryCount,
+		    retryCount = _ref$retryCount === undefined ? 1 : _ref$retryCount;
 
 		_classCallCheck(this, Wykop);
 
@@ -58,17 +54,15 @@ var Wykop = (function () {
 	}
 
 	/**
- * exportujemy klasę Wykop
- */
-
-	/**
  * Zmiana parametrów API w string
  * @param {Object} base
  * @param {Object} api parametry API
  */
 
+
 	_createClass(Wykop, [{
 		key: 'request',
+
 
 		/**
   * Tworzenie requestu do API
@@ -81,37 +75,38 @@ var Wykop = (function () {
 		value: function request(rtype, rmethod) {
 			var _this = this;
 
-			var _ref2 = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
+			var _ref2 = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {},
+			    params = _ref2.params,
+			    api = _ref2.api,
+			    post = _ref2.post;
 
-			var params = _ref2.params;
-			var api = _ref2.api;
-			var post = _ref2.post;
-			var callback = arguments.length <= 3 || arguments[3] === undefined ? Function.prototype : arguments[3];
+			var callback = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : Function.prototype;
 
 			assert(rtype && rmethod, 'rtype and rmethod must be String and cannot be null');
 			//console.log('request: ' + rtype + ' ' + rmethod);
-			var appkey = this.appkey;
-			var secretkey = this.secretkey;
-			var userkey = this.userkey;
-			var output = this.output;
-			var format = this.format;
-			var timeout = this.timeout;
-			var useragent = this.useragent;
-			var ssl = this.ssl;
+			var appkey = this.appkey,
+			    secretkey = this.secretkey,
+			    userkey = this.userkey,
+			    output = this.output,
+			    format = this.format,
+			    timeout = this.timeout,
+			    useragent = this.useragent,
+			    ssl = this.ssl;
+
 
 			var _params = !_(params).isEmpty() ? params.join('/') + '/' : ''; // zmiana tablicy z parametrami metody w string
 			var _api = Wykop.parseApi({ appkey: appkey, userkey: userkey, output: output, format: format }, api); // zmiana obiektu z parametrami api w string
 
-			var _Wykop$parsePostParams = Wykop.parsePostParams(post);
-
-			var form = _Wykop$parsePostParams.form;
-			var formData = _Wykop$parsePostParams.formData;
-			var sortedPost = _Wykop$parsePostParams.sortedPost;
-			// parsowanie parametrów POST
+			var _Wykop$parsePostParam = Wykop.parsePostParams(post),
+			    form = _Wykop$parsePostParam.form,
+			    formData = _Wykop$parsePostParam.formData,
+			    sortedPost = _Wykop$parsePostParam.sortedPost; // parsowanie parametrów POST
 
 			// tworzymy url zapytania
-			var protocol = ssl?'https':'http';
-			var url = protocol+'://a.wykop.pl/' + rtype + '/' + rmethod + '/' + _params + _api;
+
+
+			var protocol = ssl ? 'https' : 'http';
+			var url = protocol + '://a.wykop.pl/' + rtype + '/' + rmethod + '/' + _params + _api;
 			var method = !_(post).isEmpty() ? 'POST' : 'GET';
 			var apisign = md5(secretkey + url + sortedPost);
 
@@ -146,7 +141,7 @@ var Wykop = (function () {
 							_this.login(undefined, function (err, res) {
 								_this.request(rtype, rmethod, { params: params, api: api, post: post }, callback).then(function (res) {
 									resolve(res);
-								})['catch'](function (err) {
+								}).catch(function (err) {
 									reject(err);
 								});
 							});
@@ -165,13 +160,14 @@ var Wykop = (function () {
 		/*
   * @param {String} accountkey Klucz połączenia konta z aplikacją, loguje instancję klasy Wykop nadając jej userkey
   */
+
 	}, {
 		key: 'login',
 		value: function login() {
 			var _this2 = this;
 
-			var accountkey = arguments.length <= 0 || arguments[0] === undefined ? this.accountkey : arguments[0];
-			var callback = arguments.length <= 1 || arguments[1] === undefined ? Function.prototype : arguments[1];
+			var accountkey = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : this.accountkey;
+			var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : Function.prototype;
 
 			assert(accountkey, 'accountkey cannot be null');
 			return this.request('User', 'Login', { post: { accountkey: accountkey } }).then(function (res) {
@@ -187,7 +183,7 @@ var Wykop = (function () {
 	}], [{
 		key: 'parseApi',
 		value: function parseApi(base, api) {
-			var keys = undefined;
+			var keys = void 0;
 			_.assign(base, api);
 			keys = _(base).omit(_.isUndefined).omit(_.isNull).keys();
 			return _(keys).reduce(function (memo, key, index) {
@@ -197,11 +193,11 @@ var Wykop = (function () {
 	}, {
 		key: 'parsePostParams',
 		value: function parsePostParams() {
-			var post = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+			var post = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
-			var form = undefined,
-			    formData = undefined,
-			    sortedPost = undefined;
+			var form = void 0,
+			    formData = void 0,
+			    sortedPost = void 0;
 			if (post.embed && typeof post.embed !== 'string') {
 				formData = post;
 				post = _.omit(post, 'embed');
@@ -216,6 +212,11 @@ var Wykop = (function () {
 	}]);
 
 	return Wykop;
-})();
+}();
+
+/**
+* exportujemy klasę Wykop
+*/
+
 
 module.exports = Wykop;
